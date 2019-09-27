7e1221cd18092d8bc97d5e6f52532cc8271dbbdc
Rob Newling <rnewling@gmail.com>
2019-09-27 10:08:14 -0400

    improved logging, set port from appsettings file

--- NaveegoGrpcPlugin/NaveegoGrpcPlugin/Program.cs
+++ NaveegoGrpcPlugin/NaveegoGrpcPlugin/Program.cs
@@ -5,16 +5,46 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace NaveegoGrpcPlugin
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            AppDomain.CurrentDomain.ProcessExit += ProcessExitHandler;
-            CreateHostBuilder(args).Build().Run();
+
+            Log.Logger = new LoggerConfiguration()
+               .MinimumLevel.Debug()
+               //.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+               .Enrich.FromLogContext()
+               //.WriteTo.Console()
+               .WriteTo.File("PluginLog.txt", rollingInterval: RollingInterval.Day)
+               .CreateLogger();
+
+            try
+            {
+                AppDomain.CurrentDomain.ProcessExit += ProcessExitHandler;
+                var configuration = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json")
+                        .Build();
+                CreateHostBuilder(args, configuration).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
         }
 
         private static void ProcessExitHandler(object sender, EventArgs e)
@@ -24,17 +54,19 @@ namespace NaveegoGrpcPlugin
 
         // Additional configuration is required to successfully run gRPC on macOS.
         // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        public static IHostBuilder CreateHostBuilder(string[] args, IConfiguration configuration) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.ConfigureKestrel(options =>
                     {
+                        var port = configuration.GetValue<int>("GrpcPort");
                         // Setup a HTTP/2 endpoint without TLS.
-                        options.ListenLocalhost(50051, o => o.Protocols =
+                        options.ListenLocalhost(port, o => o.Protocols =
                             HttpProtocols.Http2);
                     });
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSerilog();
                 });
     }
 }
--- NaveegoGrpcPlugin/NaveegoGrpcPlugin/Startup.cs
+++ NaveegoGrpcPlugin/NaveegoGrpcPlugin/Startup.cs
@@ -5,13 +5,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace NaveegoGrpcPlugin
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -20,13 +27,13 @@ namespace NaveegoGrpcPlugin
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime applicationLifetime)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            Console.WriteLine("50051");
+            app.UseSerilogRequestLogging();
 
             app.UseRouting();
 
@@ -39,6 +46,20 @@ namespace NaveegoGrpcPlugin
                     await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
                 });
             });
+
+            applicationLifetime.ApplicationStarted.Register(ShowPort);
+
+            
+        }
+
+        private void ShowPort()
+        {
+            var port = Configuration.GetValue<int>("GrpcPort").ToString();
+            if (!string.IsNullOrEmpty(port))
+            {
+                Console.WriteLine(port);
+            }
+            
         }
     }
 }
