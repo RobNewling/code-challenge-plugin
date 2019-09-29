83ee9349f0593911f1ac658d05c4f7a93b17d4aa
Rob Newling <rnewling@gmail.com>
2019-09-29 17:26:46 -0400

    remove unneeded comments

--- NaveegoGrpcPlugin/NaveegoGrpcPlugin/Startup.cs
+++ NaveegoGrpcPlugin/NaveegoGrpcPlugin/Startup.cs
@@ -19,14 +19,11 @@ namespace NaveegoGrpcPlugin
         {
             Configuration = configuration;
         }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime applicationLifetime)
         {
             if (env.IsDevelopment())
@@ -49,7 +46,6 @@ namespace NaveegoGrpcPlugin
 
             applicationLifetime.ApplicationStarted.Register(ShowPort);
 
-            
         }
 
         private void ShowPort()
